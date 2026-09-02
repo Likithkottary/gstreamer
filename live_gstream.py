06e4a31b5401110f7c894161e63c5b6e24cafdf0
Likith Kottary <Likithkottary@users.noreply.github.com>
2026-09-02 15:06:31 +0530

    gstream cpu

--- live_gstream.py
+++ live_gstream.py
@@ -1,12 +1,11 @@
 import gi
 gi.require_version("Gst", "1.0")
 from gi.repository import Gst, GLib
-
 from cameras import cameras
 
 Gst.init(None)
 
-def run_cpu():
+def run_cpu(display=True):
 
     pipelines = []
 
@@ -14,14 +13,18 @@ def run_cpu():
 
         print(f"Starting CPU: {name}")
 
+        if display:
+            sink = "videoconvert ! autovideosink sync=false"
+        else:
+            sink = "fakesink"
+
         pipeline = Gst.parse_launch(
             f'''
             rtspsrc location="{url}" latency=100 !
             rtph265depay !
             h265parse !
             avdec_h265 !
-            videoconvert !
-            autovideosink
+            {sink}
             '''
         )
 
@@ -42,4 +45,5 @@ def run_cpu():
         for pipeline in pipelines:
             pipeline.set_state(Gst.State.NULL)
 
-run_cpu()
+
+run_cpu(display=True)
